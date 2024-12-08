2bb5e64581172b44fd088a20f7e8c545c7438ca4
bharathgopu <30866947+bharathgopu@users.noreply.github.com>
2024-12-07 23:36:11 -0500

    Capstone_Project_APP.py

--- Capstone_Project_APP.py
+++ Capstone_Project_APP.py
@@ -80,7 +80,7 @@ def load_models(model_paths, model_name_mapping):
 
 # Define image preprocessing
 def transform_image(image):
-    transform = transforms.Compose([
+    transform = transforms.Compose([ 
         transforms.Resize((512, 512)),
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
@@ -145,6 +145,7 @@ def load_bounding_boxes(file_path):
 
 
 bounding_boxes = load_bounding_boxes("imgIdToBBoxArray.p")
+
 
 # Streamlit Interface
 st.title("Drone Segmentation for Rescue and Defence")
